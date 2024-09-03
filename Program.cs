a8f7d73aa47480cbf1d79b0f941fa0656ada49ca
NurzhanMakhmut <154623160+NurzhanMakhmut@users.noreply.github.com>
2024-09-03 23:34:18 +0000

    add code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] inputStrings = { "Hello", "2", "world", ":-)" };
+        string[] outputStrings = FilterShortStrings(inputStrings);
+        Console.WriteLine("[" + string.Join(", ", outputStrings) + "]");
+    }
+
+    static string[] FilterShortStrings(string[] strings)
+    {
+        int count = 0;
+        foreach (string s in strings)
+        {
+            if (s.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        int index = 0;
+
+        foreach (string s in strings)
+        {
+            if (s.Length <= 3)
+            {
+                result[index] = s;
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
